dc2120f505b55f203093391be83dd8faea32f5cf
Coutlaw <cass.d.outlaw@gmail.com>
2021-06-15 12:57:40 -0400

    finished

--- images/src/api/imgur.js
+++ images/src/api/imgur.js
@@ -1,5 +1,7 @@
 import qs from 'qs';
+import axios from 'axios';
 
+const CLIENT_ID = '';
 const ROOT_URL = 'https://api.imgur.com'
 
 export default {
@@ -10,5 +12,26 @@ export default {
         };
 
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(queryString)}`;
+    },
+
+    fetchImages(authToken) {
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${authToken}`
+            }
+        })
+    },
+    uploadImages(authToken, images) {
+        const promises = Array.from(images).map( image => {
+            const formData = new FormData();
+            formData.append('image', image);
+            return axios.post(`${ROOT_URL}/3/image`, formData, {
+                headers: {
+                    Authorization: `Bearer ${authToken}`
+                }
+            })
+        })
+
+        return Promise.all(promises)
     }
 }
